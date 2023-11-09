1a4a8ccaa1edbaa305b4adec4505b27199cf91cc
Julio Estrada <juliojr281001@gmail.com>
2023-11-08 23:11:05 -0600

    Add images

---
+++ form.js
@@ -0,0 +1,14 @@
+"use strict";
+
+//Inputs
+const nombreInput = document.querySelector("#form6Example1");
+const apellidosInput = document.querySelector("#form6Example2");
+const direccionInput = document.querySelector("#form6Example4");
+const emailInput = document.querySelector("#form6Example5");
+const telefonoInput = document.querySelector("#form6Example6");
+const infoAdicionalInput = document.querySelector("#form6Example7");
+
+//Btns
+const enviarBtn = document.querySelector(".enviar-btn");
+
+enviarBtn.addEventListener("click", function () {});
