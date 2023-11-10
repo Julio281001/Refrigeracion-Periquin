5504b0901ae1e3951bea563b290d505ef8a75853
Julio Estrada <juliojr281001@gmail.com>
2023-11-09 19:17:12 -0600

    Validate form inputs

--- form.js
+++ form.js
@@ -11,4 +11,56 @@ const infoAdicionalInput = document.querySelector("#form6Example7");
 //Btns
 const enviarBtn = document.querySelector(".enviar-btn");
 
-enviarBtn.addEventListener("click", function () {});
+//Form
+const form = document.querySelector("form");
+
+//Functions
+function requiredFieldsAreFilled(nombre, telefono) {
+  return nombre !== "" && telefono !== "" ? true : false;
+}
+
+function validInputs(inputs) {
+  const [nombre, apellidos, direccion, email, telefono] = inputs;
+  if (!/^[a-zA-Z]+$/.test(nombre.trim())) {
+    //Validar Nombre
+    alert("Nombre invalido");
+    return false;
+  }
+  if (!/^[a-zA-Z\s]*$/.test(apellidos.trim())) {
+    //Validar apellidos
+    alert("Apellidos Invalido");
+    return false;
+  }
+  if (!/^[\w\s]*(?:\s*#\s*\d+)?$/.test(direccion.trim())) {
+    //Validar dirección
+    alert("dirección invalida");
+    return false;
+  }
+  if (!/^\d{10}$/.test(telefono.trim())) {
+    //Validar Télefono
+    return false;
+  }
+
+  return true;
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!requiredFieldsAreFilled(nombreInput.value, telefonoInput.value)) {
+    alert("Asegurate de llenar los campos requeridos");
+    return;
+  }
+  if (
+    !validInputs([
+      nombreInput.value,
+      apellidosInput.value,
+      direccionInput.value,
+      emailInput.value,
+      telefonoInput.value,
+    ])
+  ) {
+    alert("invalid");
+    return;
+  }
+  alert("Valid");
+});
